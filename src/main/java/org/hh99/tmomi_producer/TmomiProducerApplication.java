45da3ee7e0344bb52d6471ac2270d359ac868a0d
heroq <96118954+heroq@users.noreply.github.com>
2024-02-14 22:57:16 +0900

    Feat: init

---
+++ src/main/java/org/hh99/tmomi_producer/TmomiProducerApplication.java
@@ -0,0 +1,13 @@
+package org.hh99.tmomi_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmomiProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmomiProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hh99/tmomi_producer/TmomiProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hh99.tmomi_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmomiProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
