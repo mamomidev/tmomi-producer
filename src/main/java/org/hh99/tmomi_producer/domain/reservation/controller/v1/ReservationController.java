47863731e20c810d5e9a90f611c6843df377d914
mum <mumum3521@naver.com>
2024-02-16 21:25:50 +0900

    Style : controller restcontroller로 변경

--- src/main/java/org/hh99/tmomi_producer/domain/reservation/controller/v1/ReservationController.java
+++ src/main/java/org/hh99/tmomi_producer/domain/reservation/controller/v1/ReservationController.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class ReservationController {
