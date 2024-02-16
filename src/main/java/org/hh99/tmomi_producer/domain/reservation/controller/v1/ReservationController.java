c4137a5653ff50480e0a4c3076495b6fa8c97269
heroq <96118954+heroq@users.noreply.github.com>
2024-02-16 20:53:34 +0900

    Feat: 카프카 send 추가

--- src/main/java/org/hh99/tmomi_producer/domain/reservation/controller/v1/ReservationController.java
+++ src/main/java/org/hh99/tmomi_producer/domain/reservation/controller/v1/ReservationController.java
@@ -1,16 +1,22 @@
 package org.hh99.tmomi_producer.domain.reservation.controller.v1;
 
+import org.hh99.tmomi_producer.domain.reservation.service.ReservationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class ReservationController {
 
-	@PostMapping("/eventTimes/{eventTimeId}/reservation")
-	public void reservation(@PathVariable Long eventTimeId){
+	private final ReservationService reservationService;
 
+	@PostMapping("/eventTimes/{eventTimeId}/reservation")
+	public void reservation(@PathVariable Long eventTimeId) {
+		reservationService.reservationEventIdSend(eventTimeId);
 	}
 }
--- src/main/java/org/hh99/tmomi_producer/domain/reservation/service/ReservationService.java
+++ src/main/java/org/hh99/tmomi_producer/domain/reservation/service/ReservationService.java
@@ -1,8 +1,16 @@
 package org.hh99.tmomi_producer.domain.reservation.service;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ReservationService {
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+public class ReservationService {
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public void reservationEventIdSend(Long eventTimeId) {
+		kafkaTemplate.send("reservationEventTimeId" + eventTimeId, "자리를 주세요");
+	}
 }
