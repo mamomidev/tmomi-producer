90656df3f4f11f4e7aa7b3417d68083b1714246c
heroq <96118954+heroq@users.noreply.github.com>
2024-03-01 01:47:38 +0900

    Feat: AWS IAM 추가

--- src/main/java/org/hh99/tmomi_producer/global/config/KafkaConfig.java
+++ src/main/java/org/hh99/tmomi_producer/global/config/KafkaConfig.java
@@ -20,16 +20,22 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 public class KafkaConfig {
 	@Value(value = "${spring.kafka.bootstrap-servers}")
 	private String bootstrapAddress;
+	@Value(value = "${aws.access-key}")
+	private String awsAccessKeyId;
+	@Value(value = "${aws.secret-access-key}")
+	private String awsSecretAccessKey;
 
 	private final String TOPIC_NAME = "reservation";
 
 	@Bean
 	public ProducerFactory<String, ReservationDto> producerFactory() {
-		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-		return new DefaultKafkaProducerFactory<>(configProps);
+		Map<String, Object> config = new HashMap<>();
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		config.put("AWS_ACCESS_KEY_ID", awsAccessKeyId);
+		config.put("AWS_SECRET_ACCESS_KEY", awsSecretAccessKey);
+		return new DefaultKafkaProducerFactory<>(config);
 	}
 
 	@Bean
