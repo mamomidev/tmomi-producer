748353481b0c4f69c8d0d2ead0d19f92005eeefe
TaeHoon0 <kimtahwn@naver.com>
2024-03-04 04:39:42 +0900

    Refactor : consumer test 수정

--- src/main/java/org/hh99/tmomi_producer/global/config/KafkaConfig.java
+++ src/main/java/org/hh99/tmomi_producer/global/config/KafkaConfig.java
@@ -20,10 +20,6 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 public class KafkaConfig {
 	@Value(value = "${spring.kafka.bootstrap-servers}")
 	private String bootstrapAddress;
-	@Value(value = "${aws.access-key}")
-	private String awsAccessKeyId;
-	@Value(value = "${aws.secret-access-key}")
-	private String awsSecretAccessKey;
 
 	private final String TOPIC_NAME = "reservation";
 
@@ -34,14 +30,6 @@ public class KafkaConfig {
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
-		System.setProperty("AWS_ACCESS_KEY_ID", awsAccessKeyId);
-		System.setProperty("AWS_SECRET_ACCESS_KEY", awsSecretAccessKey);
-
-		config.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
-		config.put(SaslConfigs.SASL_MECHANISM, "AWS_MSK_IAM");
-		config.put(SaslConfigs.SASL_JAAS_CONFIG, "software.amazon.msk.auth.iam.IAMLoginModule required;");
-		config.put(SaslConfigs.SASL_CLIENT_CALLBACK_HANDLER_CLASS,
-			"software.amazon.msk.auth.iam.IAMClientCallbackHandler");
 
 		return new DefaultKafkaProducerFactory<>(config);
 	}
