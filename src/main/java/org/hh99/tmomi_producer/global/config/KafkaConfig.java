5c3d50eb29935a82b777c39159f64d09994c170d
heroq <96118954+heroq@users.noreply.github.com>
2024-03-08 18:28:51 +0900

    Fix: AWS MSK IAM 삭제

--- src/main/java/org/hh99/tmomi_producer/global/config/KafkaConfig.java
+++ src/main/java/org/hh99/tmomi_producer/global/config/KafkaConfig.java
@@ -3,9 +3,7 @@ package org.hh99.tmomi_producer.global.config;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.hh99.reservation.dto.ReservationDto;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,10 +18,6 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 public class KafkaConfig {
 	@Value(value = "${spring.kafka.bootstrap-servers}")
 	private String bootstrapAddress;
-	@Value(value = "${aws.access-key}")
-	private String awsAccessKeyId;
-	@Value(value = "${aws.secret-access-key}")
-	private String awsSecretAccessKey;
 
 	@Bean
 	public ProducerFactory<String, ReservationDto> producerFactory() {
@@ -31,15 +25,6 @@ public class KafkaConfig {
 		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
-//		System.setProperty("AWS_ACCESS_KEY_ID", awsAccessKeyId);
-//		System.setProperty("AWS_SECRET_ACCESS_KEY", awsSecretAccessKey);
-//
-//		config.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
-//		config.put(SaslConfigs.SASL_MECHANISM, "AWS_MSK_IAM");
-//		config.put(SaslConfigs.SASL_JAAS_CONFIG, "software.amazon.msk.auth.iam.IAMLoginModule required;");
-//		config.put(SaslConfigs.SASL_CLIENT_CALLBACK_HANDLER_CLASS,
-//				"software.amazon.msk.auth.iam.IAMClientCallbackHandler");
 
 		return new DefaultKafkaProducerFactory<>(config);
 	}
